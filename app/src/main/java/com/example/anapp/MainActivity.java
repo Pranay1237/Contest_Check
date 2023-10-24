d31d58ad35923cf7502d98ef9992ce28f3141c8e
Pranay1237 <mudhirajpranay30@gmail.com>
2023-10-24 23:03:58 +0530

    Made the edit button in the main screen to add new tasks

--- app/src/main/java/com/example/anapp/MainActivity.java
+++ app/src/main/java/com/example/anapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.anapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton button = findViewById(R.id.EditButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
